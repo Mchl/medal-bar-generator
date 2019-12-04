cca64cc91e78a1cbe5f25b72d36990b811d36f60
Michal Jarosz <michal.jarosz@stepstone.com>
2019-12-05 00:22:44 +0100

    Fix artifacts from non-integer divisions on vertical grille pattern

--- patterns/verticalGrille.js
+++ patterns/verticalGrille.js
@@ -1,8 +1,8 @@
 const verticalGrille = (canvas) => {
   const ctx = canvas.getContext('2d')
   ctx.fillStyle = `rgba(0, 0, 0, 0.25)`
-  for (let y = canvas.height / 64; y < canvas.height; y+= canvas.height / 16) {
-    ctx.fillRect(0, y, canvas.width, canvas.height / 32)
+  for (let y = Math.round(canvas.height / 64); y < canvas.height; y+= Math.round(canvas.height / 16)) {
+    ctx.fillRect(0, y, canvas.width, Math.round(canvas.height / 32))
   }
 }
 
